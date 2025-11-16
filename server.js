b8e05471ec090a0e73dfce40a472e7beac95ada3
rudrakshbhati789 <133377582+rudrakshbhati789@users.noreply.github.com>
2025-11-16 10:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-<<<<<<< HEAD
+
 import fs from "fs";
 
 // ROUTES
-=======
 
->>>>>>> 9dd384e371295901bba4c9cc83451feb1bcd41d6
+
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
@@ -19,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
+
 // STATIC FOLDERS
 app.use("/uploads", express.static("uploads"));  // for images
 app.use(express.static("public"));              // admin-dashboard.html
@@ -30,17 +29,15 @@ files.forEach(file => {
   if (!fs.existsSync(file)) fs.writeFileSync(file, "[]");
 });
 
-// USE ROUTES
-=======
+
 app.use("/uploads", express.static("uploads"));
 app.use(express.static("public"));
 
->>>>>>> 9dd384e371295901bba4c9cc83451feb1bcd41d6
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-<<<<<<< HEAD
+
 // DEFAULT ROUTE
 app.get("/", (req, res) => {
   res.send("Param Sundari Backend Running ✔");
@@ -51,6 +48,6 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Backend server running on port ${PORT} ✔`);
 });
-=======
+
 app.listen(5000, () => console.log("Backend running on port 5000"));
->>>>>>> 9dd384e371295901bba4c9cc83451feb1bcd41d6
+
